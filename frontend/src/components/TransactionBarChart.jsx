44ed83e05848803ea0d7c4a6311157e44f5d7fc2
Rajarajan <rajarajancps@gmail.com>
2024-07-28 15:38:13 +0530

    Final Commit

--- frontend/src/components/TransactionBarChart.jsx
+++ frontend/src/components/TransactionBarChart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const TransactionBarChart = ({ barChart }) => {
   const data = barChart?.barChartData;
